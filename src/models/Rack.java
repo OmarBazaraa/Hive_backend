0074ce55cac5c1469eeb5f9e643337b398eb0b6c
Omar <omar_bazaraa@hotmail.com>
2019-02-21 16:13:55 +0200

    Update Rack model class

--- src/models/Rack.java
+++ src/models/Rack.java
@@ -1,5 +1,43 @@
 package models;
 
+import utils.Position;
+
 public class Rack {
 
+    //
+    // Member Variables
+    //
+
+    private int mRow, mCol;
+    private int mItemId;
+    private int mItemsCount;
+
+    // ===============================================================================================
+    //
+    // Public Member Functions
+    //
+
+    public Rack(int r, int c) {
+        this.mRow = r;
+        this.mCol = c;
+        this.mItemsCount = 0;
+        this.mItemId = -1;
+    }
+
+    // ===============================================================================================
+    //
+    // Public Getters & Setters
+    //
+
+    public Position getPosition() {
+        return new Position(this.mRow, this.mCol);
+    }
+
+    public int getItemId() {
+        return this.mItemId;
+    }
+
+    public int getItemsCount() {
+        return this.mItemsCount;
+    }
 }
