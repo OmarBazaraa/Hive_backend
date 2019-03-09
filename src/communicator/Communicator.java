bbd65ff59bc58d822a165571700b3a9e75eefa54
Omar <omar_bazaraa@hotmail.com>
2019-03-09 22:17:41 +0200

    Add dummy communicator class

---
+++ src/communicator/Communicator.java
@@ -0,0 +1,13 @@
+package communicator;
+
+
+/**
+ * This {@code Communicator} class is the connection between our Hive Warehouse System's
+ * backend and frontend.
+ * <p>
+ * It contains useful functions for sending and receiving information between the backend and the frontend.
+ */
+public class Communicator {
+
+    // TODO
+}
