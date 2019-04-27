d9170b2862700f31f0b26fb84f7758ecfae13513
Ahmed Samir <discere044@icloud.com>
2019-04-28 01:03:30 +0200

    Add initial algorithm for the strongly NP-hard Task Creation Problem.

--- src/algorithms/Dispatcher.java
+++ src/algorithms/Dispatcher.java
@@ -11,6 +11,7 @@ import models.tasks.Task;
 import models.warehouses.Warehouse;
 import utils.Pair;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -86,148 +87,186 @@ public class Dispatcher {
     /**
      * WIP
      * TODO @Samir55 complete the Docs here upon function implementation and optimization completion.
-     * TODO @Samir55 Selects the best {@code Rack} according to a specific heuristic search.
+     * TODO @Samir55, Either remove useless racks or prevent sending them at the first place.
      * TODO @Samir55 Polish, Improve & Choose better variable names.
      *
      * @param gatePos         todo
-     * @param racks           todo
+     * @param racksList       todo
      * @param items           todo
      * @param itemsQuantities todo
      * @return rack the most suitable {@code Rack}s for fulfilling the order found in the list.
      */
-    private static List<Integer> selectRacks(Position gatePos, Map<Integer, Rack> racks, List<Item> items, int[] itemsQuantities) {
-        // TODO @Samir55, Either remove useless racks or prevent sending them at the first place.
-        // Initialization
-        List<Integer> selRacks = new ArrayList<>();
-        List<Integer> selRacksCost = new ArrayList<>();
-        int[] selRacksQuantities = new int[items.size()];
-        Double cost = 0.0;
+    private static List<Rack> selectRacks(Position gatePos, List<Rack> racksList, List<Item> items, int[] itemsQuantities) {
+        int itemsCount = items.size();
 
-        List<Integer> candiRacks = new ArrayList<>();
-        List<Integer> uselessRacks = new ArrayList<>();
-        List<Integer> tripCost = new ArrayList<>();
+        Map<Rack, Integer> candidateRacks = new HashMap<>(); // Map of candidate racks and their round trip cost
+        Map<Rack, Integer> ignoredRacks = new HashMap<>();
+        Map<Rack, Integer> acceptedRacks = new HashMap<>();
 
-        for (Map.Entry<Integer, Rack> rack : racks.entrySet())
-            candiRacks.add(rack.getKey());
+        int[] acceptedRacksItemsQuantities = new int[itemsCount];
+        int estCost = 0;
 
         // Calculate the round trip cost using Manhattan distance between each rack and the Gate position
-        for (int i = 0; i < racks.size(); i++) {
-            Position rPos = racks.get(candiRacks.get(i)).getPosition();
-            tripCost.add(2 * (Math.abs(rPos.row - gatePos.row) + Math.abs(rPos.col - gatePos.col)));
-        }
-
-        // Get the number of quantities left
-        Integer q = 0;
-        for (int i = 0; i < items.size(); i++)
-            q += itemsQuantities[i];
+        for (Rack rack : racksList)
+            candidateRacks.put(rack, 2 * (Math.abs(rack.getPosition().row - gatePos.row) +
+                    Math.abs(rack.getPosition().col - gatePos.col)));
 
         int[] quantities = itemsQuantities.clone();
+        int totalQuantities = Arrays.stream(quantities).sum();
 
-        while (q > 0 && candiRacks.size() > 0) {
+        // Stage 1: Find an initial solution
+        while (totalQuantities > 0 && candidateRacks.size() > 0) {
+            Map<Rack, int[]> racksItemsQuantities = new HashMap<>(); // TODO @Samir55 CHANGE NAME HERE.
 
-            // Calculate the maximum quantity of order items that can be taken out of each candidate rack
-            int k = candiRacks.size();
-            int[][] p = new int[k][items.size()];
-            int[] b = new int[k];
-            double[] costRate = new double[k];
+            Rack bestRack = null;
+            double bestRate = 1e9;
 
-            int argMin = -1;
-            double lrate = 1e9;
+            for (Map.Entry<Rack, Integer> rackEntry : candidateRacks.entrySet()) {
+                Rack rack = rackEntry.getKey();
 
-            for (int i = 0; i < k; i++) {
-                Rack rack = racks.get(candiRacks.get(i));
+                // Calculate the maximum needed quantity of order items that can be taken out of each candidate rack
+                racksItemsQuantities.put(rack, rackItemsSupply(rack, items, itemsQuantities));
 
-                for (int j = 0; j < items.size(); j++) {
-                    p[i][j] = Math.min(quantities[j], rack.get(items.get(j)));
-                    b[i] += p[i][j];
-                }
+                int rackTotalItemSupply = Arrays.stream(racksItemsQuantities.get(rack)).sum();
 
-                // Useless rack, mark to be removed.
-                if (b[i] == 0) {
-                    uselessRacks.add(candiRacks.get(i));
+                // Ignore rack, doesn't offer new items to the current accepted racks set
+                if (rackTotalItemSupply == 0) {
+                    ignoredRacks.put(rack, rackEntry.getValue());
+                    candidateRacks.remove(rack);
                 }
 
                 // Calculate the rate of this rack
-                costRate[i] = 1. * tripCost.get(i) / b[i];
+                double rackCostRate = 1. * candidateRacks.get(rack) / rackTotalItemSupply;
 
-                if (Double.compare(costRate[i], lrate) < 0) {
-                    lrate = costRate[i];
-                    argMin = i;
+                if (Double.compare(rackCostRate, bestRate) < 0) {
+                    bestRate = rackCostRate;
+                    bestRack = rack;
                 }
             }
 
-            // Select the rack possessing the least cost rate.
-            selRacks.add(candiRacks.get(argMin));
-            selRacksCost.add(tripCost.get(argMin));
+            // Accept the best rack.
+            assert bestRack != null;
+            int bestRackCost = candidateRacks.get(bestRack);
+            acceptedRacks.put(bestRack, bestRackCost);
+            estCost += bestRackCost;
 
-            cost += tripCost.get(argMin);
+            // Remove from the candidate racks list
+            candidateRacks.remove(bestRack);
 
-            // Remove from the candidate racks list.
-            candiRacks.remove(argMin);
-            tripCost.remove(argMin);
-
-            // Update the left quantities of the orders and the total selected racks quantities.
-            q = 0;
-            for (int j = 0; j < items.size(); j++) {
-                quantities[j] -= p[argMin][j];
-                q += quantities[j];
-
-//                selRacksQuantities[j] += p[argMin][j];
-                selRacksQuantities[j] += racks.get(candiRacks.get(argMin)).get(items.get(j));
-            }
-
-            // Prepare the new candiRacks list.
-            List<Integer> newTripCost = new ArrayList<>();
-            List<Integer> newCandiRacks = new ArrayList<>();
-            int l = 0;
-            for (int i = 0; i < candiRacks.size(); i++) {
-                if (uselessRacks.get(l).equals(candiRacks.get(i)))
-                    l++;
-                else {
-                    newCandiRacks.add(candiRacks.get(i));
-                    newTripCost.add(tripCost.get(i));
-                }
-            }
-            candiRacks = newCandiRacks;
-            tripCost = newTripCost;
-        }
-
-        // Stage 2. Delete Redundant racks from the final set.
-        List<Integer> finalSet = new ArrayList<>(); // TODO @Samir55 Polishing required here.
-        List<Integer> finalSetCost = new ArrayList<>();
-
-        for (int i = 0; i < selRacks.size(); i++) {
-            if (!redundantRack(racks.get(selRacks.get(i)), items, selRacksQuantities, itemsQuantities)) {
-                finalSet.add(selRacks.get(i));
-                finalSetCost.add(selRacksCost.get(i));
+            // Update the left quantities of the orders items and the total selected racks quantities
+            totalQuantities = 0;
+            for (int j = 0; j < itemsCount; j++) {
+                quantities[j] -= racksItemsQuantities.get(bestRack)[j];
+                totalQuantities += quantities[j];
+                acceptedRacksItemsQuantities[j] += bestRack.get(items.get(j));
             }
         }
 
-        // Stage 3. Improve the final rack list; Exchange strategy.
+        // Stage 2. Delete Redundant racks from the final set
+        removeRedundantRack(acceptedRacks, candidateRacks, items, acceptedRacksItemsQuantities, itemsQuantities,
+                true);
 
-        return finalSet;
+        // Stage 3. Improve the final rack list; Exchange strategy
+        Map<Rack, Integer> tmpCandidateRacks = new HashMap<>();
+        estCost = exchangeRacks(candidateRacks, acceptedRacks, tmpCandidateRacks, acceptedRacksItemsQuantities, items,
+                itemsQuantities, estCost);
+        estCost = exchangeRacks(ignoredRacks, acceptedRacks, tmpCandidateRacks, acceptedRacksItemsQuantities, items,
+                itemsQuantities, estCost);
+
+        return new ArrayList<>(acceptedRacks.keySet());
     }
 
     /**
      * WIP
-     * <p>
-     * Helper Function to the main task creation algorithm.
-     * Check whether the rack is redundant to the the current selected racks list.
+     * Apply Exchange step to the current accepted racks with the ignored and candidate racks for better refinements if
+     * possible.
      *
-     * @param rack                {@code Rack} rack in question.
+     * @param rackSet                 HashMap racks where we want to exchange each of its racks with ones in the accepted racks.
+     * @param acceptedRacks           HashMap racks the accepted racks needs refinements.
+     * @param tmpCandidateRacks       HashMap racks where we put the removed racks from the accepted racks due to exchange operation.
+     * @param accRacksItemsQuantities List of the total quantities of order items found in the current selected @{code Rack}s.
+     * @param items                   List of the orders @{code Item}s.
+     * @param orderQuantities         List of the needed orders @{code Item}s quantities.
+     * @param estCost                 Estimated round trip costs of the current accepted racks.
+     */
+    private static int exchangeRacks(Map<Rack, Integer> rackSet, Map<Rack, Integer> acceptedRacks,
+                                     Map<Rack, Integer> tmpCandidateRacks, int[] accRacksItemsQuantities,
+                                     List<Item> items, int[] orderQuantities, int estCost) {
+        for (Map.Entry<Rack, Integer> rackEntry : rackSet.entrySet()) {
+            Rack rack = rackEntry.getKey();
+            int[] q = accRacksItemsQuantities.clone();
+
+            for (int j = 0; j < q.length; j++)
+                q[j] += rack.get(items.get(j));
+
+            if (Arrays.equals(q, accRacksItemsQuantities)) // Not making any differences
+                continue;
+
+            // Check whether it's worth updating
+            int savedCost = removeRedundantRack(acceptedRacks, tmpCandidateRacks, items, q, orderQuantities, false); // FIXME @Samir55
+
+            if (savedCost > rackEntry.getValue()) {
+                removeRedundantRack(acceptedRacks, tmpCandidateRacks, items, q, orderQuantities, true);
+                estCost = estCost + rackEntry.getValue() - savedCost;
+            }
+
+        }
+        return estCost;
+    }
+
+    /**
+     * WIP
+     * Helper Function to the main task creation algorithm.
+     * Calculate the cost of the redundant racks and Remove the redundant racks found in the src map
+     * and put them in the dest map if allowed.
+     *
+     * @param src                 HashMap racks map in question where we want to calculate the saved cost of the redundant.
+     * @param dest                HashMap racks map where to out the removed racks if allowed.
      * @param items               List of the orders @{code Item}s.
      * @param currRacksQuantities List of the total quantities of order items found in the current selected @{code Rack}s.
      * @param orderQuantities     List of the needed orders @{code Item}s quantities.
-     * @return boolean The rack in question is redundant or not.
+     * @param updateMaps          boolean Remove the redundant racks from the src and put in the dest or not.
+     * @return Integer the saved cost.
      */
-    private static boolean redundantRack(Rack rack, List<Item> items, int[] currRacksQuantities, int[] orderQuantities) {
-        boolean redundant = true;
+    private static int removeRedundantRack(Map<Rack, Integer> src, Map<Rack, Integer> dest, List<Item> items, int[] currRacksQuantities, int[] orderQuantities, boolean updateMaps) {
+        int savedCost = 0;
 
-        for (int j = 0; redundant && j < orderQuantities.length; j++) {
-            if (currRacksQuantities[j] - rack.get(items.get(j)) < orderQuantities[j]) // FIXME @Samir55
-                redundant = false;
+        for (Map.Entry<Rack, Integer> rackEntry : src.entrySet()) {
+            Rack rack = rackEntry.getKey();
+
+            boolean redundant = true;
+
+            for (int j = 0; redundant && j < orderQuantities.length; j++) {
+                if (currRacksQuantities[j] - rack.get(items.get(j)) < orderQuantities[j])
+                    redundant = false;
+            }
+
+            if (redundant) {
+                savedCost += rackEntry.getValue();
+                if (updateMaps) {
+                    dest.put(rack, rackEntry.getValue());
+                    src.remove(rack);
+                }
+            }
         }
-
-        return redundant;
+        return savedCost;
     }
+
+    /**
+     * Get the maximum needed quantities of order items that can be taken out of each candidate rack.
+     *
+     * @param rack            @{code Rack} rack in question.
+     * @param items           List list of the orders @{code Item}s.
+     * @param orderQuantities int[] array of the needed quantities.
+     * @return int[] the maximum needed quantities of order items that can be taken out of each candidate rack.
+     */
+    private static int[] rackItemsSupply(Rack rack, List<Item> items, int[] orderQuantities) {
+        int[] ret = new int[orderQuantities.length];
+
+        for (int j = 0; j < items.size(); j++)
+            ret[j] = Math.min(orderQuantities[j], rack.get(items.get(j)));
+
+        return ret;
+    }
+
 }
