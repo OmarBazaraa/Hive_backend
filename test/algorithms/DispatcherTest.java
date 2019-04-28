60e34af05daa2d7d967989853652c620b4400849
Ahmed Samir <discere044@icloud.com>
2019-04-28 02:19:51 +0200

    Add Dispatcher Test Class Skeleton.

---
+++ test/algorithms/DispatcherTest.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import algorithms.Dispatcher;
+
+public class DispatcherTest {
+    @Test
+    public void testRackSelection() {
+
+        Dispatcher dispatcher = new Dispatcher();
+
+        // Test Rack Selection.
+        assertTrue(true);
+    }
+
+}
+
