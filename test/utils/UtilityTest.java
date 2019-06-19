d81f2a1e3598963662327d656229223099ec5a21
Omar <omar_bazaraa@hotmail.com>
2019-06-19 13:56:17 +0200

    Add simple direction manipulation test

--- test/utils/UtilityTest.java
+++ test/utils/UtilityTest.java
@@ -1,6 +1,10 @@
 package utils;
 
+import utils.Constants.*;
+
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 
 public class UtilityTest {
@@ -11,5 +15,22 @@ public class UtilityTest {
     @BeforeClass
     public static void before() throws Exception {
 
+    }
+
+    @Test
+    public void directionTest() {
+        Direction dir;
+
+        dir = Utility.nextDir(Direction.DOWN, AgentAction.ROTATE_LEFT);
+        Assert.assertEquals(dir, Direction.RIGHT);
+
+        dir = Utility.prevDir(Direction.RIGHT, AgentAction.ROTATE_LEFT);
+        Assert.assertEquals(dir, Direction.DOWN);
+
+        dir = Utility.nextDir(Direction.RIGHT, AgentAction.ROTATE_RIGHT);
+        Assert.assertEquals(dir, Direction.DOWN);
+
+        dir = Utility.prevDir(Direction.DOWN, AgentAction.ROTATE_RIGHT);
+        Assert.assertEquals(dir, Direction.RIGHT);
     }
 }
